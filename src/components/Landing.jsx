fc25684d64f44bc154937640417b18e73ca3a829
Laura Rodríguez <laurarguezfdez@gmail.com>
2024-09-16 20:23:19 +0200

    Add button and footer styles to landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,10 +2,13 @@ import imgMoney from '../images/money.png';
 
 function Landing() {
   return (
-    <section className='landing'>
-      <p className='landing_title'>Track smarter, </p>
-      <p className='landing_title'>spend better</p>
-    </section>
+    <>
+      <section className='landing'>
+        <p className='landing_title'>Track smarter, </p>
+        <p className='landing_title'>spend better</p>
+      </section>
+      <button className='landing_button'>Sign In</button>
+    </>
   );
 }
 
---
+++ src/components/SingIn.jsx/SingIn.jsx
@@ -0,0 +1,9 @@
+
+
+function SingIn() {
+  return (
+
+  )
+}
+
+export default SingIn;
