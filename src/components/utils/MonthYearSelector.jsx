e7f0ce4508be8833db79890fa2db270b5c71c990
Laura Rodríguez <laurarguezfdez@gmail.com>
2024-09-26 17:25:04 +0200

    Add new varstate to manage year changes

--- src/components/utils/MonthYearSelector.jsx
+++ src/components/utils/MonthYearSelector.jsx
@@ -1,21 +1,26 @@
 import { useState } from "react";
+import { IoArrowBackCircle } from "react-icons/io5";
+import { IoArrowForwardCircle } from "react-icons/io5";
+
 
 function MonthYearSelector() {
 
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear()); //Año actual
   const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth()); //Mes actual
   const [showCalendar, setShowCalendar] = useState(false); //Para controlar la visibilidad del calendario
+  const [calendarYear, setCalendarYear ] = useState(currentYear); //Año en el calendario (mientras se navega por los años)
   
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
   
-  //Función para cambiar el año
+  //Función para cambiar el año del calendario
   const changeYear = (amount) => {
-    setCurrentYear(currentYear + amount);
+    setCalendarYear(calendarYear + amount);
   };
   
   //Función para alternar la visibilidad del calendario
   const toggleCalendar = () => {
     setShowCalendar(!showCalendar);
+    setCalendarYear(currentYear);
   };
   
   return (
@@ -27,17 +32,18 @@ function MonthYearSelector() {
       {showCalendar && (
         <div className="calendar">
           <div className="calendar_header">
-            <button className="calendar_header_btn" onClick={() => changeYear(-1)}>◀</button>
-            <h2 className="calendar_header_year">{currentYear}</h2>
-            <button className="calendar_header_btn" onClick={() => changeYear(1)}>▶</button>
+            <IoArrowBackCircle className="calendar_header_btn_icon" onClick={() => changeYear(-1)}/>
+            <h2 className="calendar_header_year">{calendarYear}</h2>
+            <IoArrowForwardCircle className="calendar_header_btn_icon" onClick={() => changeYear(1)}/>
           </div>
           <div className="calendar_months">
             {months.map((month, index) => (
               <div 
                 key={index} 
-                className={`calendar_month ${selectedMonth === index ? 'selected' : ''}`} 
+                className={`calendar_month ${selectedMonth === index ? 'month_selected' : ''}`} 
                 onClick={() => {
                   setSelectedMonth(index); //Selecciona el mes
+                  setCurrentYear(calendarYear); //Actualiza el año que se muestra en el botón
                   toggleCalendar(); //Cierra el calendario al seleccionar
                 }}
                 >{month}
